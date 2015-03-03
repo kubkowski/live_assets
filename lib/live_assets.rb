a0b4bd103de71cfb3c656ce0eebb67c69b4e8fba
kubkowski <jakub.opara@gmail.com>
2015-03-03 21:36:14 +0100

    Added timer and eager loading

--- lib/live_assets.rb
+++ lib/live_assets.rb
@@ -3,18 +3,28 @@ require "thread"
 require "listen"
 
 module LiveAssets
-    autoload :SSESubscriber, "live_assets/sse_subscriber"
+    extend ActiveSupport::Autoload
+
+    eager_autoload do
+        autoload :SSESubscriber
+    end
+
     mattr_reader :subscribers
     @@subscribers = []
+    @@mutex = Mutex.new
 
     # Subscribe to all published events.
     def self.subscribe(subscriber)
-        subscribers << subscriber
+        @@mutex.synchronize do
+            subscribers << subscriber
+        end
     end
 
     # Unsubscribe an existing subscriber.
     def self.unsubscribe(subscriber)
-        subscribers.delete(subscriber)
+        @@mutex.synchronize do
+            subscribers.delete(subscriber)
+        end
     end
 
     # Start a listener for the following directories.
@@ -28,4 +38,12 @@ module LiveAssets
         end
     end
 
+    def self.start_timer (event, time)
+        Thread.new do
+            while true
+                subscribers.each { |s| s << event }
+                sleep(time)
+            end
+        end
+    end
 end
--- lib/live_assets/engine.rb
+++ lib/live_assets/engine.rb
@@ -1,5 +1,7 @@
 module LiveAssets
     class Engine < ::Rails::Engine
+        config.eager_load_namespaces << LiveAssets
+
         initializer "live_assets.start_listener" do |app|
             paths = app.paths["app/assets"].existent +
                     app.paths["lib/assets"].existent +
@@ -9,6 +11,12 @@ module LiveAssets
 
             if app.config.assets.compile
                 LiveAssets.start_listener :reloadCSS, paths
+            end
+        end
+
+        initializer "live_assets.start_timer" do |app|
+            if app.config.assets.compile
+                LiveAssets.start_timer :ping, 10
             end
         end
     end
